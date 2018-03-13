11c86b20713b46dd0b8df5d7c898103c500de7dd
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2018-03-13 22:11:21 +0100

    rename solution to sprotyfy

--- SprotyFy/SprotFy.Controller/Controllers/PlayListController.cs
+++ SprotyFy/SprotFy.Controller/Controllers/PlayListController.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace EventEoursingWorkshop.API.Controllers
+namespace SprotFy.Controller.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class PlayListController : Microsoft.AspNetCore.Mvc.Controller
     {
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new [] { "value1", "value2" };
         }
 
         // GET api/values/5
---
+++ SprotyFy/SprotFy.Controller/Events/PlayListCreated.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SprotFy.Controller.Events
+{
+    public class PlayListCreated
+    {
+        public string Uresid { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
