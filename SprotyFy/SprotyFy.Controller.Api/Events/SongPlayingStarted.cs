6e777d320a55a721af7766241e1aa03a7e84056e
Kim Van Renterghem <kim.vanrenterghem@t4t.rocks>
2018-03-13 23:14:53 +0100

    adding start and stop play event

---
+++ SprotyFy/SprotyFy.Controller.Api/Events/SongPlayingStarted.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SprotyFy.Controller.Api.Events
+{
+    public class SongPlayingStarted
+    {
+        public string Userid { get; set; }
+        public Guid SongId { get; set; }
+        public int Sec { get; set; }
+        
+    }
+}
---
+++ SprotyFy/SprotyFy.Controller.Api/Events/SongPlayingStoped.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SprotyFy.Controller.Api.Events
+{
+    public class SongPlayingStoped
+    {
+        public string Userid { get; set; }
+        public Guid SongId { get; set; }
+        public int Sec { get; set; }
+    }
+}
